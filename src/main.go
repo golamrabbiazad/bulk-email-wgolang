74c66d64fcd9363a40d316939987e340517dc603
golamrabbiazad <golamrabbiazad@gmail.com>
2021-10-03 09:16:20 +0600

    syncronized version & exportCSV feature added

--- src/main.go
+++ src/main.go
@@ -2,38 +2,63 @@ package main
 
 import (
 	citynames "bulk-email/src/cityNames"
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+var data = [][]string{}
+
 func main() {
 	allCities := citynames.CityNames()
 
 	fmt.Println("printing emails")
-	for _, val := range allCities {
-		res, err := http.Get("http://publicemailrecords.com/city/" + val + "/Arkansas")
 
-		if err != nil {
-			log.Fatal(err)
-		}
+	file, err := os.Create("result.csv")
+
+	checkError("Cannot create file", err)
+
+	defer file.Close()
+
+	for _, val := range allCities {
+
+		res, err := http.Get("http://publicemailrecords.com/city/" + val + "/Arkansas")
+		data = append(data, strings.Fields(val))
+
+		checkError("Cannot get url", err)
 
 		defer res.Body.Close()
 
 		doc, err := goquery.NewDocumentFromReader(res.Body)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError("response body not parsed", err)
 
 		doc.Find(".container .tbl-sect").Each(func(i int, s *goquery.Selection) {
 			s.Find("div.email.email-data.mk-link").Each(func(j int, q *goquery.Selection) {
 				text := q.Text()
-				fmt.Println(strings.TrimSpace(text))
+				data = append(data, strings.Fields(strings.TrimSpace(text)))
 			})
 		})
+
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	fmt.Println("Ready to export in CSV")
+	for _, val1 := range data {
+		err := writer.Write(val1)
+		checkError("Cannot write to file", err)
+	}
+}
+
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
 	}
 }
